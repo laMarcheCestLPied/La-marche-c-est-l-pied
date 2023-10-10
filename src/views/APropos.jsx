00055de073850e9fbec460d1fe286765ee62bc6a
Felix Karma <romain_wyon@orange.fr>
2023-10-10 16:41:58 +0200

    A Propos update

--- src/views/APropos.jsx
+++ src/views/APropos.jsx
@@ -1,32 +1,44 @@
-import React from 'react';
+import React from "react";
 
 const APropos = () => {
     return (
         <section className="m-3">
             <h1 className="fw-bold">A propos de nous</h1>
             <p>
-                Nous sommes une association de marche basée dans la région et composée de
-                personnes de tous âges et de tous horizons.
+                Nous sommes une association de marche basée dans la région et composée
+                de personnes de tous âges et de tous horizons.
+            </p>
+
+            <p>
+                En effet, le club fut créé le 24 septembre 1998 avec une déclaration
+                officielle en date du 17 octobre 1998, sous l'impulsion de Germaine
+                ROUSSEAU, Présidente, avec l'aide de Bernadette BOULAS et Daniel
+                MAAMESIE. Puis, se succédèrent: Didier DELCROIX avec Danièle comme
+                secrétaire, Jean-Michel BILLOIR, Andrée FOURNIER, Christophe PERLOT et
+                Delphnie STACHOWSKI. En février 2014, l'annonce de l'organisation du
+                premier interclub fut faite. Celui-ci eut lieu le 18 mai 2014 à Rouvroy
+                en proposant 2 parcours de 7 et 12 kms, comme c'est encore le cas
+                aujourd'hui. Le club comptait à l'époque environ 25 adhérents.
             </p>
 
             <p>
                 Notre mission est de promouvoir les bienfaits de la marche pour la santé
                 physique et mentale, tout en offrant une occasion de rencontrer de
                 nouvelles personnes et de découvrir les merveilles de notre région. Nous
-                organisons régulièrement des sorties de marche en groupe, adaptées à tous
-                les niveaux de condition physique et à toutes les préférences de parcours.
-                Les lieux de départ et les horaires des sorties de marche sont disponibles
-                sur notre site internet, ainsi que les niveaux de difficulté des parcours
-                proposés.
+                organisons régulièrement des sorties de marche en groupe, adaptées à
+                tous les niveaux de condition physique et à toutes les préférences de
+                parcours. Les lieux de départ et les horaires des sorties de marche sont
+                disponibles sur notre site internet, ainsi que les niveaux de difficulté
+                des parcours proposés.
             </p>
 
             <p>
-                En plus de notre engagement pour la santé, nous nous attachons à créer une
-                communauté dynamique et chaleureuse. Nous sommes convaincus que la marche
-                est un moyen formidable de se connecter avec les autres et de tisser des
-                liens durables. Nous organisons également des événements spéciaux, comme
-                des week-ends de marche ou des randonnées thématiques, pour offrir encore
-                plus de possibilités de rencontres et de découvertes.
+                En plus de notre engagement pour la santé, nous nous attachons à créer
+                une communauté dynamique et chaleureuse. Nous sommes convaincus que la
+                marche est un moyen formidable de se connecter avec les autres et de
+                tisser des liens durables. Nous organisons également des événements
+                spéciaux, comme des week-ends de marche ou des randonnées thématiques,
+                pour offrir encore plus de possibilités de rencontres et de découvertes.
             </p>
 
             <p>
@@ -43,7 +55,8 @@ const APropos = () => {
                 tarifs et les avantages réservés aux membres sont disponibles sur notre
                 site internet. Nous sommes fiers de rassembler des personnes de toutes
                 origines, âges et cultures, et les témoignages de membres actuels ou
-                anciens de l&apos;association attestent de la richesse de cette diversité.
+                anciens de l&apos;association attestent de la richesse de cette
+                diversité.
             </p>
 
             <p>
@@ -52,8 +65,8 @@ const APropos = () => {
                 importants ou encore l&apos;ouverture à de nouveaux publics. Nous sommes
                 convaincus que la marche est une pratique qui a de l&apos;avenir et nous
                 espérons vous rencontrer bientôt lors de l&apos;une de nos sorties pour
-                partager avec vous notre amour pour la marche et pour la vie en mouvement
-                !
+                partager avec vous notre amour pour la marche et pour la vie en
+                mouvement !
             </p>
         </section>
     );
