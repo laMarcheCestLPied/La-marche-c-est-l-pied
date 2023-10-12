3d74440193b194f64111ec3d05f60f52443b5ef5
Felix Karma <romain_wyon@orange.fr>
2023-10-12 16:14:14 +0200

    redirect rectif

--- src/views/RedirectToHome.jsx
+++ src/views/RedirectToHome.jsx
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react';
 
 const RedirectToHome = () => {
     useEffect(() => {
-        if (window.location.pathname !== '/La-marche-c-est-l-pied' ) {
+        if (window.location.pathname !== '/' && !window.location.pathname.startsWith('/votre-autre-route')) {
             // Si l'URL n'est pas la page d'accueil et ne correspond pas à une autre route définie
-            window.location.href = '/La-marche-c-est-l-pied'; // Rediriger vers la page d'accueil
+            window.location.href = '/'; // Rediriger vers la page d'accueil
         }
     }, []);
 
