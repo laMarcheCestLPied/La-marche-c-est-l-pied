8181ddc0c9fcad02572c1f954a0598b2c5202343
Felix Karma <romain_wyon@orange.fr>
2023-10-16 16:25:08 +0200

    redirect good

--- src/views/RedirectToHome.jsx
+++ src/views/RedirectToHome.jsx
@@ -3,9 +3,10 @@ import { useEffect } from 'react';
 const RedirectToHome = () => {
     console.log(window.location.pathname);
     useEffect(() => {
-        if (window.location.pathname !== '/La-marche-c-est-l-pied/' ) {
+        if (window.location.pathname !== '/La-marche-c-est-l-pied' && window.location.pathname !== '/La-marche-c-est-l-pied/' ) {
             // Si l'URL n'est pas la page d'accueil et ne correspond pas à une autre route définie
-            window.location.href = 'https://lamarchecestlpied.github.io/La-marche-c-est-l-pied/'; // Rediriger vers la page d'accueil
+            window.location.href = 'https://lamarchecestlpied.github.io/La-marche-c-est-l-pied'; // Rediriger vers la page d'accueil
+            // window.location.href = 'http://localhost:3000/La-marche-c-est-l-pied'; // Rediriger vers la page d'accueil
         }
     }, []);
 
