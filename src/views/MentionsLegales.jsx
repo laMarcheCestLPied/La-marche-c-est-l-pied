4d6c546dc3a30f0f734795284a597cc7f2946dba
FelixKarma <romain_wyon@orange.fr>
2025-09-08 15:43:06 +0200

    suppression tel

--- src/views/MentionsLegales.jsx
+++ src/views/MentionsLegales.jsx
@@ -15,7 +15,7 @@ const MentionsLegales = () => {
                 </h6>
                 <p>Nom de l&apos;association : La marche c&apos;est l&apos;pied</p>
                 <p>Adresse : <a href='https://www.google.com/maps/dir//50.393427,2.903673/' className='text-dark'>5 rue de la Mairie, 62320 Rouvroy</a></p>
-                <p>Numéro de téléphone : <span className='fw-bold'>06.09.01.52.87 </span> - <span className='fw-bold'>06.61.04.70.56</span></p>
+                <p>Numéro de téléphone : <span className='fw-bold'>06.09.01.52.87 </span></p>
                 <p>Adresse e-mail : <span className='text-primary'>lamarcheclepied@laposte.net</span></p>
 
                 {/* <h6 className="text-decoration-underline mt-4">Hébergement du site :</h6>
